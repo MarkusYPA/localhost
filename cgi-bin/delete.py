786a3d36105672f0d8529feb5538ebe59c06acf0
Oleg Balandin <oleg.balandin@gritlab.ax>
2025-10-20 20:10:12 +0300

    gitignore logs

--- cgi-bin/delete.py
+++ cgi-bin/delete.py
@@ -8,7 +8,7 @@ import datetime
 UPLOAD_DIR = './www/uploads'
 
 def log(message):
-    with open('/Users/oleg.balandin/Desktop/work/localhost/python.log', 'a') as f:
+    with open('delete.log', 'a') as f:
         f.write(f"{datetime.datetime.now()}: {message}\n")
 
 def main():
